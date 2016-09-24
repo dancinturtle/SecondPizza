a4d1b6e176e7b6d12f2fe363e8e81804910e8114
Sharol Chand <sharolchand@gmail.com>
2016-09-24 16:52:21 -0700

    fixing KENNY's stuff

--- client/js/factories/loginFactory.js
+++ client/js/factories/loginFactory.js
@@ -1,1 +1,18 @@
-loginFactory.js
+donationApp.factory('loginFactory', function($http){
+ // var categories = [{name:'array'}, {name: 'string'}, {name:'binary search tree' }, {name:'singly link list' }];
+  var factory = {};
+
+// factory.getCategory = function(callback){
+//   callback(categories)
+// }
+//
+// factory.add = function(data, callback){
+//   $http.post('/add', data).success(function(output){
+//     console.log(output);
+//     callback(output);
+//   });
+// }
+
+
+  return factory;
+});
