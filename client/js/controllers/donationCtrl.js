3581d75079e1ab020171fbeddcc6056217c90c78
Sharol Chand <sharolchand@gmail.com>
2016-09-24 16:48:39 -0700

    database connection works, you can get users out

--- client/js/controllers/donationCtrl.js
+++ client/js/controllers/donationCtrl.js
@@ -1,4 +1,9 @@
-donationApp.controller('donationCtrl', function ($scope, donationFactory) {
+donationApp.controller('donationCtrl', function ($scope, donationFactory, businessFactory) {
+
+    businessFactory.getUsers(function(data){
+        $scope.categories = data;
+        console.log(data);
+  });
 
 
 });
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -12,21 +12,31 @@ var con = mysql.createConnection({
 module.exports = (function(){
   return {
     create: function(req, res){
-      console.log("Posted", req.body);
-      var newuser = {first_name: req.body.firstname, last_name: req.body.lastname, username: req.body.username, email: req.body.email, password: req.body.password, salt: req.body.salt, phone: req.body.phone, home_lat: req.body.home_lat, home_long: req.body.home_long, image_path: req.body.image_path, visiting_lat: req.body.visiting_lat, visiting_long: req.body.visiting_long, created_at: new Date(), updated_at: new Date()};
-      con.query('INSERT INTO users SET ?', newuser, function(err, result){
-        if(err){
-          res.json(err);
-        }
-        else {
-          res.json(result);
-        }
-      })
+
+        // console.log("Posted", req.body);
+        //  var newuser = {name: req.body.name, password: req.body.lastname, username: req.body.username, email: req.body.email, password: req.body.password, salt: req.body.salt, phone: req.body.phone, home_lat: req.body.home_lat, home_long: req.body.home_long, image_path: req.body.image_path, visiting_lat: req.body.visiting_lat, visiting_long: req.body.visiting_long, created_at: new Date(), updated_at: new Date()};
+        //  con.query('INSERT INTO users SET ?', newuser, function(err, result){
+        //    if(err){
+        //      res.json(err);
+        //    }
+        //    else {
+        //      res.json(result);
+        //    }
+        //  })
+    //   console.log("Posted", req.body);
+    //   var newuser = {first_name: req.body.firstname, last_name: req.body.lastname, username: req.body.username, email: req.body.email, password: req.body.password, salt: req.body.salt, phone: req.body.phone, home_lat: req.body.home_lat, home_long: req.body.home_long, image_path: req.body.image_path, visiting_lat: req.body.visiting_lat, visiting_long: req.body.visiting_long, created_at: new Date(), updated_at: new Date()};
+    //   con.query('INSERT INTO users SET ?', newuser, function(err, result){
+    //     if(err){
+    //       res.json(err);
+    //     }
+    //     else {
+    //       res.json(result);
+    //     }
+    //   })
     },
 
 
     index: function(req, res){
-
       con.query('SELECT * FROM users', function(err, rows){
         if(err){
           res.json(err);
