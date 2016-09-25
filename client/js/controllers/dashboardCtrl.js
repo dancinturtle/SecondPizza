75c3c3c555b742dc1b4ed7168ab13772365a69b3
dancinturtle <amy.giver@gmail.com>
2016-09-25 08:50:02 -0700

    string dates

--- client/js/controllers/dashboardCtrl.js
+++ client/js/controllers/dashboardCtrl.js
@@ -1,8 +1,11 @@
 donationApp.controller('dashboardCtrl', function ($scope, $routeParams, $location, dashboardFactory) {
   dashboardFactory.index(function(data){
     $scope.companies = data;
+    for (var i=0; i<$scope.companies.length; i++){
+        var date = new Date($scope.companies[i].expiration_date);
+        $scope.companies[i].expiration_date = date.toDateString();
+      }
 
-  })
+    })
 
-
-});
+  });
