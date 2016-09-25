6343f4de28c35dc74663b227cf9d94848784aeb2
Sharol Chand <sharolchand@gmail.com>
2016-09-25 06:31:28 -0700

    tweet shown per donation

--- client/js/controllers/businessCtrl.js
+++ client/js/controllers/businessCtrl.js
@@ -1,16 +1,40 @@
-donationApp.controller('businessCtrl', function ($scope, $routeParams, userFactory, $location) {
+donationApp.controller('businessCtrl', function ($scope, $routeParams, userFactory, $location, TwitterService) {
 
   $scope.business_info;
   $scope.business_id = $routeParams.id;
   console.log("business id", $scope.business_id);
 
+  $scope.business_tweets = [];
 
-
-  userFactory.getdonations($scope.business_id, function(data){
-
-      console.log('adadaddad', data);
+  userFactory.getdonations($routeParams.id, function(data){
       $scope.business_info = data;
   })
+
+  // All donation tweets
+  $scope.getDonationTweets = function(){
+      userFactory.getdonations($routeParams.id, function(data){
+          $scope.business_info = data;
+            for (var business in $scope.business_info) {
+                $scope.business_tweets.push($scope.getSearch($scope.business_info[business].hashtag));
+            }
+      });
+  }
+
+  $scope.getDonationTweets();
+
+  // Get Tweets by hashtag
+  $scope.getSearch = function(hashtag){
+      TwitterService.getSearch(hashtag)
+      .then(function(data){
+          $scope.twitterErrors = undefined;
+          $scope.hashtagData = JSON.parse(data.result.hashData);
+          console.log($scope.hashtagData);
+      })
+      .catch(function(error){
+          console.error('there was an error retrieving data: ', error);
+          $scope.twitterErrors = error.error;
+      })
+  }
 
 
 });
