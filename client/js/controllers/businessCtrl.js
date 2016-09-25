b7e2a3aadcc76de89c3d9c9a40426f873bdf04a9
dancinturtle <amy.giver@gmail.com>
2016-09-25 07:19:44 -0700

    working on tweet display

--- client/js/controllers/businessCtrl.js
+++ client/js/controllers/businessCtrl.js
@@ -1,10 +1,9 @@
-donationApp.controller('businessCtrl', function ($scope, $routeParams, userFactory, $location, TwitterService) {
+donationApp.controller('businessCtrl', function ($scope, $routeParams, userFactory, $location) {
 
   $scope.business_info;
   $scope.business_id = $routeParams.id;
   console.log("business id", $scope.business_id);
 
-<<<<<<< HEAD
   $scope.mstohuman = function(date){
     var diff = new Date(date) - new Date();
     var hours = 0;
@@ -13,10 +12,7 @@ donationApp.controller('businessCtrl', function ($scope, $routeParams, userFacto
     var minutes = 0;
     diff = Math.floor(diff / 60000); //how many minutes ago
     while(diff >= 0){
-      // if(5 > diff && diff >= 0){
-      //   minutes = "Less than 5";
-      //   diff = -1;
-      // }
+  
       if(60 > diff && diff >= 0){
         minutes = diff;
         diff=-1;
@@ -36,43 +32,17 @@ donationApp.controller('businessCtrl', function ($scope, $routeParams, userFacto
     }
     return {weeks: weeks, days: days, hours: hours, minutes: minutes}
   }
-=======
-  $scope.business_tweets = [];
->>>>>>> c811d34a9fd8554e35a51601a76b4b38031aca37
 
-  userFactory.getdonations($routeParams.id, function(data){
+
+  userFactory.getdonations($scope.business_id, function(data){
+
+      console.log('adadaddad', data);
       $scope.business_info = data;
       for(var i=0;i<$scope.business_info.length; i++){
         var diff = $scope.mstohuman($scope.business_info[i].expiration_date);
         $scope.business_info[i].timeLeft = diff;
       }
   })
-
-  // All donation tweets
-  $scope.getDonationTweets = function(){
-      userFactory.getdonations($routeParams.id, function(data){
-          $scope.business_info = data;
-            for (var business in $scope.business_info) {
-                $scope.business_tweets.push($scope.getSearch($scope.business_info[business].hashtag));
-            }
-      });
-  }
-
-  $scope.getDonationTweets();
-
-  // Get Tweets by hashtag
-  $scope.getSearch = function(hashtag){
-      TwitterService.getSearch(hashtag)
-      .then(function(data){
-          $scope.twitterErrors = undefined;
-          $scope.hashtagData = JSON.parse(data.result.hashData);
-          console.log($scope.hashtagData);
-      })
-      .catch(function(error){
-          console.error('there was an error retrieving data: ', error);
-          $scope.twitterErrors = error.error;
-      })
-  }
 
 
 });
